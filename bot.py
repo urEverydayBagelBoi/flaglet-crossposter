9a0a93ba5fca1bccf1f05dd8a57d5b516ac3ff0f
urEverydayBagelBoi <joanirisakotta@proton.me>
2026-05-05 17:56:10 +0200

    Rename Discord bits

--- bot.py
+++ bot.py
@@ -3,35 +3,35 @@ import logging
 from interactions import Client, Intents, listen
 from interactions.api.events import Component
 
+# Read .env (tokens, etc.)
 from py_dotenv import read_dotenv
 import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 read_dotenv(dotenv_path)
 
-
 logging.basicConfig()
-flaglet_log = logging.getLogger("FlagletLogger")
-flaglet_log.setLevel(logging.DEBUG)
+discord_log = logging.getLogger("FlagletLogger")
+discord_log.setLevel(logging.DEBUG)
 
-client = Client(
+discord_client = Client(
     intents=Intents.DEFAULT | Intents.MESSAGE_CONTENT,
     asyncio_debug=True,
-    logger=flaglet_log
+    logger=discord_log
 )
 
 @listen()
 async def on_ready():
-    flaglet_log.info(f"Ready. Logged in as {client}\nOwned by {client.owner}")
+    discord_log.info(f"Ready. Logged in as {discord_client}\nOwned by {discord_client.owner}")
 
 @listen()
 async def on_message_create(event):
-    # flaglet_log.debug(f"Message received: {event.message.content}")
+    # discord_log.debug(f"Message received: {event.message.content}")
     content = event.message.content
-    if not event.message.author.id == client.user.id and ("#art " in content or content.endswith("#art")):
+    if not event.message.author.id == discord_client.user.id and ("#art " in content or content.endswith("#art")):
         attachment_urls = " ".join([attachment.url for attachment in event.message.attachments])
         msg = f"Original Message: {event.message.jump_url}\nAuthor: {event.message.author.mention}\n\n> {content}\n\n-# {attachment_urls}"
 
-        _ = await client.fetch_channel(1462914139935477814)
+        _ = await discord_client.fetch_channel(1462914139935477814)
         await _.send(msg)
 
-client.start(token=(os.getenv('DISCORD_TOKEN')))
+discord_client.start(token=(os.getenv('DISCORD_TOKEN')))
